66503352cb1308803d3eb05e778185dcc7d550a1
lqk <1501036045@qq.com>
2025-08-04 15:55:23 +0800

    initial commit

---
+++ pages/index/index.vue
@@ -0,0 +1,16 @@
+<template>
+	<view class="container">
+		hello
+	</view>
+</template>
+
+<script lang="ts" setup>
+	
+</script>
+
+<style>
+	.container {
+		height: 100%;
+		background-color: #f4f2ec;
+	}
+</style>
